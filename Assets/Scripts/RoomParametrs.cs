6ab6951f73113628b10c62908bc95cae8ba8eb52
SG_KaAp <kazantsev.arseniy.gamedev@gmail.com>
2025-02-22 02:33:49 +0500

    update

--- Assets/Scripts/RoomParametrs.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "Room")]
-public class RoomParametrs : ScriptableObject
-{
-    [field: SerializeField] public Vector3 NextSpawnPoint { get; private set; }
-    [field: SerializeField] public GameObject RoomPrefab { get; private set; }
-}
